93c26afa4219cb46540e9c4871684cd8a45ab30b
wohenbushuang <wohenbushuang@users.noreply.github.com>
2022-07-01 12:32:12 +0800

    Update set-CTAN-mirror.js
    version 0.3
    * match *://www.ctan.org/*
    * match *://ctan.org/*

--- set-CTAN-mirror/set-CTAN-mirror.js
+++ set-CTAN-mirror/set-CTAN-mirror.js
@@ -3,14 +3,14 @@
 // @name:zh      设置 CTAN 默认镜像
 // @name:zh-CN   设置 CTAN 默认镜像
 // @namespace    https://github.com/rnicrosoft-studio/
-// @version      0.2
+// @version      0.3
 // @description  Set to use a fixed mirror when browsing CTAN files.
 // @description:zh  设置浏览 CTAN 文件时使用一个固定的镜像。
 // @description:zh-CN   设置浏览 CTAN 文件时使用一个固定的镜像。
 // @author       rnicrosoft
 // @supportURL   https://github.com/rnicrosoft-studio/tampermonkey-scripts
-// @match        *://www.ctan.org/pkg/*
-// @match        *://ctan.org/pkg/*
+// @match        *://www.ctan.org/*
+// @match        *://ctan.org/*
 // @icon         https://www.ctan.org/assets/favicon/favicon-32x32.png
 // @run-at       document-end
 // @grant        GM_setValue
